c28623f73e97d99e65c6734e03c39be35d9e2ff1
daniel.ramirez <daniel.ramirez@smartmatic.com>
2018-03-21 01:20:34 -0400

    #N Mocha unit test example was added

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var Should = require('should');
+
+describe('my first test', function () {
+  it ('is running', function () {
+    true.should.equal(true);
+
+    return false;
+  });
+
+  return false;
+});
